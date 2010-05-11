53146598de50bd498134f3d5e924ce4f7c0d2420
MSchiemank <not@tekkub.net>
2010-05-11 18:23:40 +0200

    I can not understand it, but now it works with the types

--- GOL.hs
+++ GOL.hs
@@ -11,8 +11,13 @@ width = 20
 mark = '0'
 unmark = '.'
 
+-- nothing works with this
+type Pos = (Int, Int)
+type Cell = (Pos, Char)
+type Board = [Cell]
+
 -- The startingposition of the first lifes on the field
-startPos :: [(Int,Int)]
+startPos :: [Pos]
 -- startPos = [(3,4),(4,2),(4,3),(2,3),(4,4)]
 
 -- the f-Pentomino
@@ -20,7 +25,7 @@ startPos = [(9,10),(9,11),(10,9),(10,10),(11,10)]
 
 
 -- we are building the field. It's a list, thats like a x,y Array.
-doField :: [(Int,Int)] -> Int -> Int -> [((Int,Int),Char)]
+doField :: [Pos] -> Int -> Int -> Board
 doField pos x y | y == width = [((x,y),(fillPos pos x y))] ++ doField pos (x+1) 1  -- a new row
                 | x > hight = []                                                   -- function done
                 | otherwise = [((x,y),(fillPos pos x y))] ++ doField pos x (y+1)   -- normaly
@@ -34,29 +39,29 @@ fillPos (x:xs) y z = if x == (y,z)
 
 
 -- returns the element from the x,y Cell of the field-Array 
-gCellC:: [((Int,Int),Char)]-> (Int,Int) -> Char
+gCellC:: Board-> Pos -> Char
 gCellC f t = snd (f!!(((fst t)-1)*width+((snd t)-1)))
 
 
 -- Output on command line for the whole field
-printAll :: [((Int,Int),Char)] -> Int -> IO ()
+printAll :: Board -> Int -> IO ()
 printAll field c = do putStr (printF field 1 1 [])
                       putStrLn ("Cycle: "++show c)
                       
 
 -- generates an outputstring with a newline after a row
-printF :: [((Int,Int),Char)] -> Int -> Int -> String -> String
+printF :: Board -> Int -> Int -> String -> String
 printF field x y out | x > hight = out 
                      | y == width = out ++ [gCellC field (x,y)] ++ ['\n'] ++ printF field (x+1) 1 out
                      | otherwise = out ++ [(gCellC field (x,y))] ++ printF field x (y+1) out
 
 
 -- creating the next living list
-nextStep :: [((Int,Int),Char)] -> [(Int,Int)] -> [(Int,Int)]
+nextStep :: Board -> [Pos] -> [Pos]
 nextStep field l = (survive field l)++(born field field)
 
 -- for every item in the list will be shown, if there are 2 or 3 neighbours
-survive :: [((Int,Int),Char)] -> [(Int,Int)] -> [(Int,Int)]
+survive :: Board -> [Pos] -> [Pos]
 survive f [] = []
 survive f (x:xs) = do let cell = length (checkCell f x)
                       if  cell >=2 && cell <=3 
@@ -65,7 +70,7 @@ survive f (x:xs) = do let cell = length (checkCell f x)
 
 -- the same as survive, but it will be look in every cell and a new child will be born 
 -- if there are 2 neighbours
-born :: [((Int,Int),Char)] -> [((Int,Int),Char)] -> [(Int,Int)]
+born :: Board -> Board -> [Pos]
 born f [] = []
 born f ((a,b):xs) = do let cell = length (checkCell f a)
                        if cell == 3 
@@ -73,7 +78,7 @@ born f ((a,b):xs) = do let cell = length (checkCell f a)
                           else born f xs
 
 -- checks the eight neighbours of the cell
-checkCell :: [((Int,Int),Char)] -> (Int,Int) ->[Char]
+checkCell :: Board -> Pos ->[Char]
 checkCell f (x,y) = rmSpace (cCell f [(x-1,y-1),(x-1,y),(x-1,y+1),
                                       (x,y-1),(x,y+1),
                                       (x+1,y-1),(x+1,y),(x+1,y+1)])
@@ -86,7 +91,7 @@ rmSpace (x:xs) | x == mark = [x]++rmSpace xs
 
 -- returns the list of the neighbour. 
 -- illegal rows/columns will be filtered out
-cCell :: [((Int,Int),Char)] -> [(Int,Int)] -> [Char]
+cCell :: Board -> [Pos] -> [Char]
 cCell f [] = []
 cCell f ((a,b):xs) = if a < 1 || a > hight
                       then cCell f xs
@@ -100,7 +105,7 @@ main = mainStart startPos 1
 
 
 -- start and working function
-mainStart :: [(Int,Int)] -> Int -> IO()
+mainStart :: [Pos] -> Int -> IO()
 mainStart sl c = do let arr = doField sl 1 1
                     printAll arr c
                     let next = nextStep arr sl
