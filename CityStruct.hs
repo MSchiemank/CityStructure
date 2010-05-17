355a7e9338db40ae49e31cbafcd9139c21f571b2
MSchiemank <not@tekkub.net>
2010-05-17 19:46:43 +0200

    Inserted the comments of Michael and added the signal part. But this is difficult, so more code changing after tomorrow. After the half-an-hour-support of Michael!

--- CityStruct.hs
+++ CityStruct.hs
@@ -1,4 +1,5 @@
 import Data.List
+import Char
 --Datatype setting
 
 data Cell = Road    { ident   :: Integer,
@@ -6,58 +7,55 @@ data Cell = Road    { ident   :: Integer,
           | Building{ ident   :: Integer,
                       name    :: String}
           | Signal  { ident   :: Integer,
-                      workWith :: [Cell],
-                      against  :: [Cell]}
+                      workWith :: [Pos],
+                      against  :: [Pos]}
           | Car     { ident   :: Integer}
      deriving (Eq, Show)
 
-
---myCell = Road { ident = "R4", name = "Berliner"}
-
+type Pos = (Int, Int)
+data City = City { width  :: Integer,
+                   height :: Integer}
 
 --creates a String without the comments and the clear lines
 doInputString :: String -> [String]
-doInputString file = do let string = makeAll file
+doInputString file = do let string = makeAll file '\n'
                         rmSpace (remCommentLine (rmNullString string))
 
 
 -- remove the clear lines
 rmNullString :: [String] -> [String]
-rmNullString [] = []
-rmNullString (x:xs) | x == "" = rmNullString xs
-                    | otherwise = [x] ++ rmNullString xs
+rmNullString lines = filter (\x -> length x > 0) lines
 
 -- remove the space in the strings
 rmSpace :: [String] -> [String]
 rmSpace [] = []
-rmSpace (x:xs) = [[s | s <- x, s /= ' ']] ++ rmSpace xs
+rmSpace (x:xs) =[filter(\s -> not (isSpace s)) x] ++ rmSpace xs
 
 -- remove the comment lines
 remCommentLine :: [String] -> [String]
-remCommentLine [] = []
-remCommentLine (x:xs) | head x /= '-' = [x] ++ remCommentLine xs
-                      | otherwise = remCommentLine xs
+remCommentLine lines = filter (\x -> (head x) /='-') lines
 
 -- builds a string with each listelement is one row of the file
-makeAll :: String -> [String]
-makeAll [] = []
-makeAll file = [row] ++ makeAll (drop ((length row)+1)file)
-            where row = makeRow file
+makeAll :: String -> Char -> [String]
+makeAll [] c = []
+makeAll file c = [row] ++ makeAll (drop ((length row)+1)file) c
+                 where row = makeString file c
               
 -- selects the row of the file
-makeRow :: [Char] -> [Char]
-makeRow (x:xs) | x == '\n' = []
-               | otherwise = [x] ++ makeRow xs
+makeString :: [Char] -> Char -> [Char]
+makeString [] c = []
+makeString (x:xs) c | x == c = []
+                    | otherwise = [x] ++ makeString xs c
 
 
 --Parser is todo
 --parse :: [String] -> [[Cell]]
 {-parse s = do 
-             where hight = getHight s
-                   width = getWidth s
-                   roads = buildSegment s "Roads:"
-                   buildings = buildSegment s "Buildings:"
-                   signals = buildSegment s "Signals:"-}
+             where City {width = getWidth s,
+                         hight = getHight s}
+                   roadCells = generateRoad (searchX s ":Roads")
+                   buildingCells = generateBuilding (searchX s ":Buildings")
+                   signalCells = generateSignal (searchX s ":Signals")-}
 
 -- returns the width of the city
 getWidth :: [String] -> Int
@@ -73,43 +71,39 @@ getHight (x:xs) = if isInfixOf "hight=" x
                     then read (drop (length "hight=") x) :: Int
                     else getHight xs
 
---checks the first element of the selected lists
-buildSegment :: [String] -> String -> [((Int, Int), Cell)]
-buildSegment [] s = []
-buildSegment (x:xs) s | isInfixOf s x = endSegment xs s
-                      | otherwise = buildSegment xs s
+--checks the first element of the selected list
+searchX :: [String] -> String -> [String]
+searchX [] s = []
+searchX (x:xs) s | isInfixOf s x = endX xs s
+                      | otherwise = searchX xs s
 
 -- checks the end of the selected list
-endSegment:: [String] -> String -> [((Int, Int), Cell)]
-endSegment [] s = []
-endSegment (x:xs) s | isInfixOf "end" x = []
-                    | otherwise = if s == "Roads:" 
-                                  then generateRoad x ++ endSegment xs s
-                                  else if s == "Buildings:"
-                                        then generateBuilding x ++ endSegment xs s
-                                        else []
-
+endX:: [String] -> String -> [String]
+endX [] s = []
+endX (x:xs) s | isInfixOf ":end" x = []
+                    | otherwise = [x]  ++ endX xs s
+                                  
 
 --divide the String into 3 pices
-generateRoad :: String -> [((Int, Int), Cell)]
+generateRoad :: [String] -> [(Pos, Cell)]
 generateRoad [] = []
-generateRoad x = fillRoadCell (buildRoadList pos firstI second)
-                 where first = getStr x ';'
-                       l1 = (length first) + 1
-                       firstI = read first :: Integer
-                       second = getStr (drop l1 x) ';'
-                       l2 = (length second) + 1 + l1
-                       third = drop l2 x
-                       pos = createPosTuple (createTupleString third)
+generateRoad (x:xs) = fillRoadCell (buildRoadList pos firstI second) ++ generateRoad xs
+                       where first = getStr x ';'
+                             l1 = (length first) + 1
+                             firstI = read first :: Integer
+                             second = getStr (drop l1 x) ';'
+                             l2 = (length second) + 1 + l1
+                             third = drop l2 x
+                             pos = createPosTuple (createTupleString third)
                     
                            
 --builds a full list of roadpieces                        
-buildRoadList :: [(Int,Int)] -> Integer -> String -> [((Int, Int), Integer,String)]
+buildRoadList :: [Pos] -> Integer -> String -> [(Pos, Integer,String)]
 buildRoadList [] _ _ = []
 buildRoadList ((x1,y1):(x2,y2):zs) i s = [((x,y),i,s) | x <- [x1..x2], y <- [y1..y2]] ++ buildRoadList zs i s
 
 --fill the road cell with informations
-fillRoadCell :: [((Int,Int),Integer, String)] -> [((Int,Int), Cell)]
+fillRoadCell :: [(Pos,Integer, String)] -> [(Pos, Cell)]
 fillRoadCell [] = []
 fillRoadCell ((pos,i,s):xs) = [(pos, c)] ++ fillRoadCell xs
                                 where c = Road i s
@@ -117,37 +111,58 @@ fillRoadCell ((pos,i,s):xs) = [(pos, c)] ++ fillRoadCell xs
 
 
 --divide the buildingstring into three pices
-generateBuilding :: String -> [((Int, Int), Cell)]
-generateBuilding s = generateBuildingList pos firstI second
-                        where first = getStr s ';'
+generateBuilding :: [String] -> [(Pos, Cell)]
+generateBuilding [] = []
+generateBuilding (x:xs) = generateBuildingList pos firstI second ++ generateBuilding xs
+                        where first = getStr x ';'
                               l1 = length (first) +1
                               firstI = read first :: Integer
-                              second = getStr (drop l1 s) ';'
+                              second = getStr (drop l1 x) ';'
                               l2 = l1 + (length second) + 1
-                              third = drop l2 s
+                              third = drop l2 x
                               pos = createPosTuple (createTupleString third)
 
 --builds the List for the buildings
-generateBuildingList :: [(Int,Int)] -> Integer -> String -> [((Int,Int),Cell)]
+generateBuildingList :: [Pos] -> Integer -> String -> [(Pos,Cell)]
 generateBuildingList [] _ _ = []
 generateBuildingList (x:xs) i s = [(z, Building i s) | z <- [x]]
 
+--todo!
+--generates the signallist
+{--generateSignals :: [String] ->[(Pos, Cell)]
+generateSignals s = buildRelationships listCell listCell
+                        where listCell = genListCell s-}
 
-{--generates the signallist
-generateSignals :: String ->[((Int,Int), Cell)]
-generateSignals s = generateSignalList pos firstI rel antiRel
-                        where first = getStr s ';'
-                              i1 = (length first) + 1
-                              firstI = read first :: Integer
-                              second = getStr (drop i1 s) ';'
-                              i2 = i1 + 1 + (length second)
-                              pos = createPosTuple (createTupleString second)
-                              rel = getStr (drop i2 s) ';'
-                              i3 = i2 + 1 + (length rel)
-                              antiRel = drop i3 s
+--After the Questions of tomorrow!!!
+--builds the realationships between the signals
+--buildRelationships :: [(Pos, Cell , [Integer], [Integer])] -> [(Pos, Cell , [Integer], [Integer])] -> [(Pos,Cell)]
+--buildRelationships ((xy,Signal id with against, wId, aId):xs) l = id 
 
---generateSignalList :: [(Int,Int)] -> String -> String -> String -> [((Int,Int), [String])]
-generateSignalList (x:xs) s1 s2 s3 = (x,Signal s1 s2 s3)-}
+
+--build a list of cells without relationships
+genListCell :: [String] -> [(Pos, Cell, [Integer], [Integer])]
+genListCell [] = []
+genListCell (x:xs) = [generateEmptySignalCell pos id wW wA] ++ genListCell xs
+                            where a = makeAll x ';'
+                                  id = read (head a) :: Integer
+                                  pos = createPosTuple (createTupleString (a!!1))
+                                  wW = getIntRel (a!!2)
+                                  wA = getIntRel (a!!3)
+
+
+--builds a signal cell only with the id and also returns the other relationships
+generateEmptySignalCell :: [Pos] -> Integer -> [Integer] -> [Integer] -> (Pos, Cell, [Integer], [Integer])
+generateEmptySignalCell x i s1 s2 = (head x,(Signal i [] []),s1,s2)
+
+
+--makes the relationship from a string to an integer list
+getIntRel :: String -> [Integer]
+getIntRel s = map readI (makeAll (tail (makeString s ']')) ',')
+
+
+--returns an integer from a String
+readI :: String -> Integer
+readI s = read s :: Integer
 
 
 -- returns a string infront of the char              
@@ -163,7 +178,7 @@ createTupleString s = [str] ++ createTupleString (drop ((length str)+2) s)
                       where str = tail (getStr s ')')
 
 -- turns a tuplestring to tuples
-createPosTuple :: [String] -> [(Int,Int)]
+createPosTuple :: [String] -> [Pos]
 createPosTuple [] = []
 createPosTuple (s:xs) = [(read x1 ::Int,read y1 ::Int)] ++ createPosTuple xs
                         where x1 = getStr s ','
