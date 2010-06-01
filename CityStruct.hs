4fd69a653e5c247e4b165826f0b69eb8f8372d9e
MSchiemank <not@tekkub.net>
2010-06-01 19:31:01 +0200

    added the first run of a car and added a new start. The new starting point for the car and the endpoint of it can now be an id of a house.

--- CityStruct.hs
+++ CityStruct.hs
@@ -3,29 +3,51 @@ import Char
 
 
 
---Starting the programm with the main function
+--Starting the programm with the main function eleven times
 main :: IO()
 main = do file <- readFile "city"
-          run (parse (doInputString file))
+          let i = 10
+          run (parse (doInputString file)) i
 
 
-run :: City -> IO()
-run city = do printCity city
+run :: City -> Int -> IO()
+run _ 0 = return ()
+run city i = do printCity city
+                run (nextStep city) (i-1)
 
 
+{- The nextStep is first to generate a new dynamic list, with the moving cars on the next
+   point in the City and the switching signals.Out of this a new city will be generated.-}
+nextStep :: City -> City
+nextStep city = City {width  = getCityWidth  city,
+                      height = getCityHeight city,
+                      static = getCityStatic city,
+                      dynamic = filter (\(pos,cell) -> pos /= (-1,-1)) (map (nextCarsAndSignals) (getCityDynamic city))}
 
+
+{- The cars move along the path. The next element of the path will be the next position.
+   After the car has reached the destination, it will be removed from the city.-}
+nextCarsAndSignals :: (Pos,Cell) -> (Pos,Cell)
+nextCarsAndSignals (pos, cell) = case cell of 
+                                    { (Signal ident workWith against) -> (pos, cell);
+                                      (Car ident path)                -> carStep (pos, cell)}
+
+carStep :: (Pos,Cell) -> (Pos,Cell)
+carStep (pos,(Car id pathOld)) = if (null pathOld)
+                                    then ((-1,-1), Car {ident=0, path=[]})
+                                    else (head pathOld,Car {ident = id, path = tail(pathOld)})
 
 ------------------------------------------------------
 
 --Datatype setting
-data Cell = Road    { ident    :: Integer,
+data Cell = Road    { ident    :: Int,
                       name     :: String}
-          | Building{ ident    :: Integer,
+          | Building{ ident    :: Int,
                       name     :: String}
-          | Signal  { ident    :: Integer,
+          | Signal  { ident    :: Int,
                       workWith :: [Pos],
                       against  :: [Pos]}
-          | Car     { ident    :: Integer,
+          | Car     { ident    :: Int,
                       path     :: [Pos]}
           | Empty {}
      deriving (Eq, Show)
@@ -34,14 +56,14 @@ type Pos = (Int, Int)
 data City = City { width              :: Int,
                    height             :: Int,
                    static             :: [[Cell]],
-                   dynamic            :: [[Cell]]}
+                   dynamic            :: [(Pos,Cell)]}
      deriving (Eq, Show)
 
 --returns the contents of the structure
 getCityStatic :: City -> [[Cell]]
 getCityStatic (City width height static dynamic) = static
 
-getCityDynamic :: City -> [[Cell]]
+getCityDynamic :: City -> [(Pos,Cell)]
 getCityDynamic (City width height static dynamic) = dynamic
 
 getCityWidth :: City -> Int
@@ -68,12 +90,15 @@ printStart city width height x y | x > width = ['\n']++printStart city width hei
                                                ++printStart city width height (x+1) y
 
 printCell :: City -> Pos -> Char
-printCell city pos = if dyn /= Empty
-                        then cellToChar dyn
+printCell city pos = if not(null dyn)
+                        then cellToChar (getCellFromTuple (head dyn))
                         else cellToChar stat
-                       where dyn = getCell (getCityDynamic city) pos
+                       where dyn = filter (\(y,_) -> y==pos) (getCityDynamic city)
                              stat = getCell (getCityStatic city) pos
     
+getCellFromTuple :: (Pos,Cell) -> Cell
+getCellFromTuple (pos,cell) = cell
+
 
 getCell :: [[Cell]] -> (Int,Int) -> Cell
 getCell cell (x,y) = (cell!!(y-1))!!(x-1)
@@ -144,7 +169,7 @@ parse s = do buildCity width height (roadCells++buildingCells) (signalCells++car
                    roadCells = generateRoad (searchRows s ":Roads")
                    buildingCells = generateBuilding (searchRows s ":Buildings")
                    signalCells = generateSignals (searchRows s ":Signals")
-                   carCells = generateCarList (searchRows s ":Cars")
+                   carCells = generateCarList (searchRows s ":Cars") buildingCells roadCells
 
 -- returns the width or the height of the city
 getDim :: [String] -> String -> Int
@@ -199,7 +224,7 @@ generateRoad [] = []
 generateRoad (x:xs) = fillRoadCell (buildRoadList pos firstI second) ++ generateRoad xs
   where first = getStr x ';'
         l1 = (length first) + 1
-        firstI = read first :: Integer
+        firstI = read first :: Int
         second = getStr (drop l1 x) ';'
         l2 = (length second) + 1 + l1
         third = drop l2 x
@@ -207,14 +232,14 @@ generateRoad (x:xs) = fillRoadCell (buildRoadList pos firstI second) ++ generate
                      
                        
 --build a full list of roadpoints for one direction of one road.
-buildRoadList :: [Pos] -> Integer -> String -> [(Pos, Integer,String)]
+buildRoadList :: [Pos] -> Int -> String -> [(Pos, Int,String)]
 buildRoadList [] i s = []
 buildRoadList ((x1,y1):(x2,y2):zs) i s = if (x1 <= x2)&&(y1<=y2)
                                             then [((x,y),i,s) | x <- [x1..x2], y <- [y1..y2]] ++ buildRoadList zs i s
                                             else [((x,y),i,s) | x <- [x2..x1], y <- [y2..y1]] ++ buildRoadList zs i s
 
 --fill the road cell with informations
-fillRoadCell :: [(Pos,Integer, String)] -> [(Pos, Cell)]
+fillRoadCell :: [(Pos,Int, String)] -> [(Pos, Cell)]
 fillRoadCell [] = []
 fillRoadCell ((pos,i,s):xs) = [(pos, c)] ++ fillRoadCell xs
                                 where c = Road i s
@@ -232,14 +257,14 @@ generateBuilding [] = []
 generateBuilding (x:xs) = generateBuildingList pos firstI second ++ generateBuilding xs
                         where first = getStr x ';'
                               l1 = length (first) +1
-                              firstI = read first :: Integer
+                              firstI = read first :: Int
                               second = getStr (drop l1 x) ';'
                               l2 = l1 + (length second) + 1
                               third = drop l2 x
                               pos = createPosTuple (createTupleString third)
 
 --builds the List for the buildings and fill the cell with informations
-generateBuildingList :: [Pos] -> Integer -> String -> [(Pos,Cell)]
+generateBuildingList :: [Pos] -> Int -> String -> [(Pos,Cell)]
 generateBuildingList [] _ _ = []
 generateBuildingList (x:xs) i s = [(z, Building i s) | z <- [x]]
 
@@ -265,22 +290,22 @@ generateSignals s = buildRelationships listCell listCell
   createt with only its id. The result of the function will be a position, a cell and 
   two lists with id's. First list comprised the workWith signalid's and the second list 
   comprised the workAgainst signalid's.-}
-genListCell :: [String] -> [(Pos, Cell, [Integer], [Integer])]
+genListCell :: [String] -> [(Pos, Cell, [Int], [Int])]
 genListCell [] = []
 genListCell (x:xs) = [(head pos, (Signal id [] []), wW, wA)] ++ genListCell xs
                             where a = stringListBeforeChar x ';'
-                                  id = read (head a) :: Integer
+                                  id = read (head a) :: Int
                                   pos = createPosTuple (createTupleString (a!!1))
                                   wW = getIntRel (a!!2)
                                   wA = getIntRel (a!!3)
 
 --makes the relationship id's from a string to an integer list
-getIntRel :: String -> [Integer]
+getIntRel :: String -> [Int]
 getIntRel s = map readI (stringListBeforeChar (tail (makeString s ']')) ',')
 
 --returns an integer from a String
-readI :: String -> Integer
-readI s = read s :: Integer
+readI :: String -> Int
+readI s = read s :: Int
 
 
 
@@ -289,7 +314,7 @@ readI s = read s :: Integer
   for the returning cell and the second list is to find the coordinates of the cells
   with the right id. The id's are stored in the two integerlists. Firs integer-list is 
   the workWith-list and the second integer-list is the workAgainst-list-}
-buildRelationships :: [(Pos, Cell , [Integer], [Integer])] -> [(Pos, Cell , [Integer], [Integer])] -> [(Pos,Cell)]
+buildRelationships :: [(Pos, Cell , [Int], [Int])] -> [(Pos, Cell , [Int], [Int])] -> [(Pos,Cell)]
 buildRelationships [] l = []
 buildRelationships ((xy,Signal id workWith against, wId, aId):xs) l = [(xy, Signal {ident=id, workWith = with, against = workAgainst})] ++ buildRelationships xs l
                         where with = map (findPartner l) wId
@@ -297,7 +322,7 @@ buildRelationships ((xy,Signal id workWith against, wId, aId):xs) l = [(xy, Sign
 
 --find the partner cell with the id in the second list named ids and 
 --returns the position coordinates
-findPartner :: [(Pos, Cell, [Integer], [Integer])] -> Integer -> Pos
+findPartner :: [(Pos, Cell, [Int], [Int])] -> Int -> Pos
 findPartner ((pos, (Signal idd wW wA), i1, i2):xs) ids | idd == ids = pos
                                                        | otherwise = findPartner xs ids
 
@@ -309,34 +334,61 @@ findPartner ((pos, (Signal idd wW wA), i1, i2):xs) ids | idd == ids = pos
 
 
 {-build the car list and the path of the cars. It extracts the id, the startposition
-  and the endposition from the stringlist. Then the path of the car will be calculated
+  and the endposition from the stringlist. If there was only an id of a house, then 
+  it will be searching the nearest road of the house. This roadpiece will be the start
+  position of the car. Then the path of the car will be calculated
   with the function makePath. After all the cell will be created-}
-generateCarList :: [String] -> [(Pos, Cell)]
-generateCarList [] = []
-generateCarList (x:xs) = [(head path, Car id path)] ++ generateCarList xs
+generateCarList :: [String] -> [(Pos, Cell)] -> [(Pos, Cell)] -> [(Pos, Cell)]
+generateCarList [] _ _= []
+generateCarList (x:xs) building roads =
+                 [(head path, Car id (tail path))] ++ generateCarList xs building roads
                            where a = stringListBeforeChar x ';'
-                                 id = read (head a) ::Integer
-                                 pos = createPosTuple (createTupleString (a!!1))
-                                 end = createPosTuple (createTupleString (a!!2))
+                                 id = read (head a) ::Int
+                                 pos = findPos building roads (a!!1)
+                                 end = findPos building roads (a!!2)
                                  path = makePath pos end
 
+
+{- Extract the id of the house or the starting positon tuple-}
+findPos ::  [(Pos, Cell)] -> [(Pos, Cell)] -> String -> Pos
+findPos build street pos = if head pos=='H'
+                            then findStreetForBuilding build street (readI(tail pos))
+                            else head(createPosTuple (createTupleString pos))
+
+
+
+{- After the id of the house was found, it will search above, below, left and right
+   of the house for a road. First roadpiece will be used as startingpoint of the car.-}
+findStreetForBuilding :: [(Pos, Cell)] -> [(Pos, Cell)] -> Int -> Pos
+findStreetForBuilding build street id = head positions
+    where buildingPos = [posB | (posB, _)<-(filter (\(_,Building identB nameB) -> identB ==id) build)]
+          positions = [posS | (posS, _) <- street, (x,y) <- buildingPos,
+                       posS==(x,y-1)|| posS==(x-1,y) || posS==(x+1,y) ||
+                       posS==(x,y+1)]
+
+
+
 --first makePath function, which goes from point a to b in a line
-makePath :: [Pos] -> [Pos] -> [Pos]
-makePath ((x1,y1):xs1) ((x2,y2):xs2) = [(x,y) | x <- [x1..x2] , y <- [y1..y2]]
+makePath :: Pos -> Pos -> [Pos]
+makePath (x1,y1) (x2,y2) | x2<x1 = reverse[(x,y) | x <- [x2..x1] , y <- [y1..y2]]
+                         | y2<y1 = reverse[(x,y) | x <- [x1..x2] , y <- [y2..y1]]
+                         | otherwise = [(x,y) | x <- [x1..x2] , y <- [y1..y2]]
 
 
 
 
 
-{-now the city will be created. The two lists ar like a two dimension array
-  each row is a seperate list in the list. That guaranteed the x and y positions -}
+{-now the city will be created. The first list is like a two dimension array
+  each row is a seperate list in the list. That guaranteed the x and y positions.
+  And the second list is only a normal list, which comprised the cars and the signals
+  with the current positions. -}
 buildCity :: Int -> Int -> [(Pos,Cell)] -> [(Pos,Cell)] -> City
 buildCity w h streetBuild sigCar = City { width            = w,
                                           height           = h,
                                           static           = sB,
-                                          dynamic          = cS}
+                                          dynamic          = sigCar}
     where sB = buildList w h streetBuild
-          cS = buildList w h sigCar
+
 
 --the rows jointed together
 buildList :: Int -> Int -> [(Pos,Cell)] -> [[Cell]]
